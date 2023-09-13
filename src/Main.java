17eb04bd2867bd49353b8b4f8c144b0aac3a8724
nastia.kuz <nastenanastena180@gmail.com>
2023-09-13 23:07:23 +0300

    корректные значения входных данных

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double currentBalance = 100.0;
+
+        double rechargeAmount = 1100.0;
+
+        double bonusThreshold = 1000.0;
+
+        double bonusPer100 = 1.0;
+
+        double bonus = 0.0;
+
+
+        if (rechargeAmount > bonusThreshold) {
+
+            bonus = Math.floor((rechargeAmount - bonusThreshold) / 100) * bonusPer100;
+        }
+
+
+        double totalBalance = currentBalance + rechargeAmount + bonus;
+
+        System.out.println("Итоговый счет: " + totalBalance + " рублей");
+        System.out.println("Бонусные рубли: " + bonus + " рублей");
     }
 }
